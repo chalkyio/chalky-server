5695314a0bb3f5f94f4c5807963969904f9c9e36
Alex P <alexeek@protonmail.com>
2021-01-22 17:27:11 +0000

    fix: set sslmode to disable

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/rs/zerolog"
@@ -18,10 +19,12 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	// TODO: Use a DB other than defaultdb and a less powerful user.
-	dbURL := fmt.Sprintf("postgres://root@chalky-cockroachdb-public/defaultdb")
+	dbURL := fmt.Sprintf("postgres://root@chalky-cockroachdb-public:26257/defaultdb?sslmode=disable")
+	log.Info().Str("url", dbURL).Msg("Connecting to CockroachDB")
 	db, err = pgx.Connect(infiniteContext, dbURL)
 	if err != nil {
-		log.Fatal().Err(err).Str("url", dbURL).Msg("Failed to connect to CockroachDB")
+		log.Error().Err(err).Msg("Failed to connect to CockroachDB")
+		time.Sleep(time.Second * 50)
 	}
 
 	app := setupRouter()
