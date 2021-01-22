e1f3776abaccddd93a7a5238b704c0e766dcd600
Alex P <alexeek@protonmail.com>
2021-01-22 23:07:59 +0000

    feat: generate random uuid for user row

--- migrations/000002_create_users_table.up.sql
+++ migrations/000002_create_users_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS chalky.users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username STRING (32) NOT NULL UNIQUE,
     display_name STRING (40),
     icon STRING,
