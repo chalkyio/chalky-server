42b135b63de5121a7554e6174bb7b7abe05f20d7
Alex P <alexeek@protonmail.com>
2021-01-22 17:34:25 +0000

    dev: add hostname log

--- main.go
+++ main.go
@@ -17,12 +17,15 @@ func main() {
 	var err error
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+	hostname, _ := os.Hostname()
+	log.Info().Msg("I am " + hostname)
+
 	// TODO: Use a DB other than defaultdb and a less powerful user.
 	dbURL := fmt.Sprintf("postgres://root@chalky-cockroachdb-public:26257/defaultdb?sslmode=disable")
 	log.Info().Str("url", dbURL).Msg("Connecting to CockroachDB")
 	db, err = pgx.Connect(infiniteContext, dbURL)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to connect to CockroachDB")
+		log.Fatal().Err(err).Msg("Failed to connect to CockroachDB")
 	}
 
 	app := setupRouter()
