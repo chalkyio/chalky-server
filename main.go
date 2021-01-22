a3dd87df64b6199b848622371abdcb0bf60784b9
Alex P <alexeek@protonmail.com>
2021-01-22 16:16:23 +0000

    feat: add cockroachdb connection

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/go-pg/pg/v10"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
+var db *pg.DB
+
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	// TODO: Use a less powerful user and use a DB other than defaultdb.
+	db = pg.Connect(&pg.Options{
+		Addr:     "chalky-cockroachdb-public",
+		User:     "root",
+		Database: "defaultdb",
+	})
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatal().Err(err).Msg("Failed to ping CockroachDB")
+	}
+
 	app := setupRouter()
 
 	// TODO: Use TLS.
