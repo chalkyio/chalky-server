32bbd815abe457ef8190c2b709dbfd0c341715df
Alex P <alexeek@protonmail.com>
2021-01-22 17:45:47 +0000

    feat: add migration step

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/cockroachdb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v4"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -12,6 +15,8 @@ import (
 
 var db *pgx.Conn
 var infiniteContext = context.Background()
+
+const databaseURI = "root@chalky-cockroachdb-public:26257/defaultdb?sslmode=disable"
 
 func main() {
 	var err error
@@ -21,11 +26,22 @@ func main() {
 	log.Info().Msg("I am " + hostname)
 
 	// TODO: Use a DB other than defaultdb and a less powerful user.
-	dbURL := fmt.Sprintf("postgres://root@chalky-cockroachdb-public:26257/defaultdb?sslmode=disable")
-	log.Info().Str("url", dbURL).Msg("Connecting to CockroachDB")
-	db, err = pgx.Connect(infiniteContext, dbURL)
+	log.Info().Str("uri", databaseURI).Msg("Connecting to CockroachDB")
+	db, err = pgx.Connect(infiniteContext, "postgres://"+databaseURI)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to connect to CockroachDB")
+	}
+
+	log.Info().Msg("Migrating database")
+	migrator, err := migrate.New(
+		"file://migrations",
+		"cockroachdb://"+databaseURI,
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create migration manager")
+	}
+	if err := migrator.Up(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to migrate database")
 	}
 
 	app := setupRouter()
